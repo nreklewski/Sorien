5285e1ac3ceabed70f17446dc6ec59c7714762d2
Natan Ręklewski <natan.renke@gmail.com>
2025-07-30 17:52:17 +0200

    sitemap config adjustments

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://sorien.pl',
+  siteUrl: 'https://www.sorien.pl',
   generateRobotsTxt: true,
   changefreq: 'weekly',
   priority: 0.7,
