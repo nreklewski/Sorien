3d50b9ee6779166633b0686969a8b1bd21f4d412
Natan Ręklewski <natan.renke@gmail.com>
2025-11-08 20:47:01 +0100

    visual adjustment

--- app/blog/components/BlogHero.tsx
+++ app/blog/components/BlogHero.tsx
@@ -7,9 +7,9 @@ interface BlogHeroProps {
 
 export default function BlogHero({ imageSrc, imageAlt }: BlogHeroProps) {
   return (
-    <section className="relative w-full pt-16 pb-8 md:pt-20 md:pb-12">
+    <section className="relative w-full pb-8 md:pt-12 md:pb-12">
       {/* Decorative Background Block */}
-      <div className="absolute top-15 left-1/2 -translate-x-1/2 w-[1500px] h-[350px] md:h-[450px] bg-gradient-to-br from-[#101015] via-[#151515] to-[#101015]"></div>
+      <div className="hidden md:block absolute top-15 left-1/2 -translate-x-1/2 w-[1500px] h-[350px] md:h-[450px] bg-gradient-to-br from-[#101015] via-[#151515] to-[#101015]"></div>
 
       {/* Cover Image - Overlapping */}
       <div className="relative max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 -bottom-16 md:-bottom-20 z-10">
@@ -26,5 +26,3 @@ export default function BlogHero({ imageSrc, imageAlt }: BlogHeroProps) {
     </section>
   );
 }
-
-
